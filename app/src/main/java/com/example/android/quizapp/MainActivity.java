a051fe538c13883fc9fab39c08caa1121b9129a9
keunsulee <i.knsulee@gmail.com>
2016-08-25 01:21:47 +0900

    init

--- app/src/main/java/com/example/android/quizapp/MainActivity.java
+++ app/src/main/java/com/example/android/quizapp/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.android.quizapp;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void submitAnswer(View view) {
+        Context context = getApplicationContext();
+        String toastMessage = "Result";
+        toastMessage += "\nQuiz1: correct";
+        toastMessage += "\nQuiz2: wrong answer";
+        toastMessage += "\naaaaaaaaaa";
+        toastMessage += "\naaaaaaaaaa";
+        CharSequence text = toastMessage;
+        int duration = Toast.LENGTH_LONG;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
     }
 }
